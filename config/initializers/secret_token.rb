97657d3898303889c02ddb94304d18163f90998c
bims <pb402385@etu.unice.fr>
2013-01-13 06:12:56 -0800

    Import initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjetIFPSApp::Application.config.secret_token = '4bc82370db5e1f786d45f7fbc7b594fe38e921eaa006e8b0c23fa305f173647ec7e5c2aeb0e65fd2415e8a50712e3b7281d73cba688f930332bf75e7212714da'
